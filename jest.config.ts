6b5886dfeff9e9950c7ac348517854be2d623238
anjanibarlapati <anjani.barlapati@everst.engineering>
2024-11-07 13:41:50 +0530

    Modify jest configuration file

--- jest.config.ts
+++ jest.config.ts
@@ -2,6 +2,25 @@ import type { Config } from '@jest/types';
 
 const config: Config.InitialOptions = {
   preset: 'react-native',
+  testEnvironment: 'node', 
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  transform: {
+    '^.+\\.(ts|tsx)$': 'ts-jest',
+    '^.+\\.(js|jsx)$': 'babel-jest',
+  },
+  transformIgnorePatterns: [
+    'node_modules/(?!(@react-native|react-native|@react-native/js-polyfills)/)', 
+  ],
+  globals: {
+    'ts-jest': {
+      babelConfig: true, 
+    },
+  },
+  collectCoverage: true,
+  collectCoverageFrom: [
+    'src/**/*.{ts,tsx}',
+    '!src/**/*.test.{ts,tsx}', 
+  ],
 };
 
 export default config;
